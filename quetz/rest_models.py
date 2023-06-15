59be6c026d94b7e09d0c10aad71d5506d0235b43
Yasin Tatar <ytatar9@googlemail.com>
2023-06-15 13:44:55 +0200

    update: include condas matchspec

--- quetz/rest_models.py
+++ quetz/rest_models.py
@@ -7,7 +7,7 @@ import json
 import uuid
 from datetime import date, datetime
 from enum import Enum
-from typing import Dict, Generic, List, Optional, TypeVar
+from typing import Dict, Generic, List, Optional, TypeVar, Union
 
 from pydantic import BaseModel, Field, root_validator, validator
 from pydantic.generics import GenericModel
@@ -74,7 +74,7 @@ class ChannelBase(BaseModel):
         None, title="size limit of the channel", nullable=True
     )
     ttl: int = Field(36000, title="ttl of the channel")
-    mirror_channel_url: Optional[str | List[str]] = Field(
+    mirror_channel_url: Optional[Union[str, List[str]]] = Field(
         None, regex="^(http|https)://.+", nullable=True
     )
     mirror_mode: Optional[MirrorMode] = Field(None, nullable=True)
